1ccd2683e29f3c80eb334c18b3c38001cc8df4b6
malkiyahitesh <hiteshmalkiya8@gmail.com>
2026-08-08 23:30:41 +0530

    all ok redy for  school hub  project

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 import HeroSection from "@/components/home/Hero";
 import HomeAllschools from "@/components/home/HomeAllschools";
+import { Suspense } from "react";
 
 export default function HomePage() {
   return (
+    <Suspense fallback={<div>Loading...</div>}>
     <main>
      <HeroSection />
      <HomeAllschools />
     </main>
+    </Suspense>
   )
 }
